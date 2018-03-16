93159937f3633b7f93067852774999d2b9876a0b
Darren Evans <darrenrevans10@gmail.com>
2018-03-15 20:58:32 -0400

    updated files

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.get("*", function(req, res) {
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/transparency"
+  process.env.MONGODB_URI || "mongodb://localhost/transparency", 
+  {
+    useMongoClient: true
+  }
 );
 
 app.listen(PORT, function() {
