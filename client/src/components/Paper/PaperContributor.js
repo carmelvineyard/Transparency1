4d08365547da16130cc2e7a297810871bea590b3
Ginger Poole <gingermw@gmail.com>
2018-03-15 21:00:14 -0400

    fix text field

--- client/src/components/Paper/PaperContributor.js
+++ client/src/components/Paper/PaperContributor.js
@@ -4,8 +4,8 @@ import TableContributor from '../TableSimple/TableContributor';
 
 const style = {
   height:600,
-  width: 450,
-  margin: 20,
+  width: '45%',
+  margin: 10,
   textAlign: 'center',
   display: 'inline-block',
 };
